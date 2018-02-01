297527baa2b3a59ed62bf4bc93e024805a19cc46
r_takaishi <ryo.takaishi.0@gmail.com>
2018-02-01 22:44:35 +0900

    read config file

--- main.go
+++ main.go
@@ -2,10 +2,29 @@ package main
 
 import (
 	"fmt"
+	"github.com/BurntSushi/toml"
 	"github.com/hashicorp/consul/api"
+	"net/url"
+	"os"
 )
 
+type Config struct {
+	URL string
+}
+
 func main() {
+	var config Config
+	_, err := toml.DecodeFile(fmt.Sprintf("%s/.cnodes", os.Getenv("HOME")), &config)
+	if err != nil {
+		panic(err)
+	}
+	u, err := url.Parse(config.URL)
+	if err != nil {
+		panic(err)
+	}
+	api_config := api.DefaultConfig()
+	api_config.Address = u.Host
+	api_config.Scheme = u.Scheme
 	client, err := api.NewClient(api.DefaultConfig())
 	if err != nil {
 		panic(err)
